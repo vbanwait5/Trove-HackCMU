f229f916e8d5ccc53d7bd4e97446ad8a4e3ba864
ushukla07 <ushukla@andrew.cmu.edu>
2025-09-13 04:41:54 -0400

    Update urls.py

--- wallet/urls.py
+++ wallet/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.dashboard, name="dashboard"),
-    path("dashboard/", views.dashboard, name="dashboard"),
     path("cards/", views.cards_dashboard, name="cards"),
     path("deals/", views.perks_dashboard, name="deals"),
     path("goals/", views.spending_dashboard, name="goals"),
