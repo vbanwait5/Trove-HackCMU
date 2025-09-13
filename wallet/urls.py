65590ee690406cca387db689f47c1be8ad944ef3
ushukla07 <ushukla@andrew.cmu.edu>
2025-09-13 02:45:38 -0400

    dashboard stuff

--- wallet/urls.py
+++ wallet/urls.py
@@ -3,10 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path("dashboard/", views.dashboard, name="dashboard"),
+    path("", views.dashboard, name="dashboard"),
     path("cards/", views.cards_view, name="cards"),
     path("deals/", views.perks_dashboard, name="deals"),
     path("goals/", views.spending_dashboard, name="goals"),
     path("subscriptions/", views.subscriptions_view, name="subscriptions"),
-
 ]
